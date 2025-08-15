2e4c0ffc9de4cd03b10a755d0e80b75223f59684
rebonatokalicaa-sudo <rebonatokalicaa@gmail.com>
2025-08-14 21:13:09 -0300

    Update ex2.py
    ADD 2 section

---
+++ AULA01/ex2.py
@@ -0,0 +1,8 @@
+base = input("Insira a base do retângulo: ")
+altura = input("Insira a altura do retângulo: ")
+
+base = float(base)
+altura = float(altura)
+
+area = base * altura
+print("A área do retângulo é:", area)
