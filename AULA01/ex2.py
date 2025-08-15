08b81d5363505019eee5bf2799015cdda2e98cac
Kaique Rebonato <rebonatokalicaa@gmail.com>
2025-08-14 21:28:52 -0300

    Atualiza ex2.py com cálculo da área

--- AULA01/ex2.py
+++ AULA01/ex2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 base = input("Insira a base do retângulo: ")
 altura = input("Insira a altura do retângulo: ")
 
@@ -7,13 +6,3 @@ altura = float(altura)
 
 area = base * altura
 print("A área do retângulo é:", area)
-=======
-base = input("Insira a base do retângulo: ")
-altura = input("Insira a altura do retângulo: ")
-
-base = float(base)
-altura = float(altura)
-
-area = base * altura
-print("A área do retângulo é:", area)
->>>>>>> 2e4c0ffc9de4cd03b10a755d0e80b75223f59684
